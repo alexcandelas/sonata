a221ab08c6f3e8ed1f6f191261d8e4a21a277794
Alex Candelas <axcandelas@gmail.com>
2025-10-10 19:17:40 -0600

    Create an index file for utility function exports

---
+++ src/utils/index.js
@@ -0,0 +1,8 @@
+export { encodeSvg } from './encodeSvg.js';
+export { flattenObject } from './flattenObject.js';
+export { identsToString } from './identsToString.js';
+export { isEmptyObject } from './isEmptyObject.js';
+export { isObject } from './isObject.js';
+export { kebabCase } from './kebabCase.js';
+export { singleMerge, merge } from './merge.js';
+export { unaccent } from './unaccent.js';
