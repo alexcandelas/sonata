1ab4e3fe0cbdc8fe73d0ab08e1c08dcbbb9fdf0e
Alex Candelas <axcandelas@gmail.com>
2025-11-12 22:32:47 -0600

    Allow float values in `width` and `height` utility classes

--- src/css/utilities/sizeRules.js
+++ src/css/utilities/sizeRules.js
@@ -16,9 +16,8 @@ export const height = [
     [/^(min-|max-)?h-(max|min|fit)$/, ([_, prefix = '', property]) => ({ [`${prefix}height`]: `${property}-content` })],
     [/^(min-|max-)?h-full$/, ([_, prefix = '']) => ({ [`${prefix}height`]: '100%' })],
     [/^(min-|max-)?h-screen$/, ([_, prefix = '']) => ({ [`${prefix}height`]: '100vh' })],
-    [/^(min-|max-)?h-(\d+)$/, buildDeclaration('height')],
     [/^(min-|max-)?h-(\d+\/\d+)$/, buildDeclaration('height')],
-    [/^(min-|max-)?h-(\d+(?:\.\d+)?)([a-z]+|%)$/, buildDeclaration('height')],
+    [/^(min-|max-)?h-(\d+(?:\.\d+)?)([a-z]+|%)?$/, buildDeclaration('height')],
 ];
 
 export const width = [
@@ -27,7 +26,6 @@ export const width = [
     [/^(min-|max-)?w-(max|min|fit)$/, ([_, prefix = '', property]) => ({ [`${prefix}width`]: `${property}-content` })],
     [/^(min-|max-)?w-full$/, ([_, prefix = '']) => ({ [`${prefix}width`]: '100%' })],
     [/^(min-|max-)?w-screen$/, ([_, prefix = '']) => ({ [`${prefix}width`]: '100vw' })],
-    [/^(min-|max-)?w-(\d+)$/, buildDeclaration('width')],
     [/^(min-|max-)?w-(\d+\/\d+)$/, buildDeclaration('width')],
-    [/^(min-|max-)?w-(\d+(?:\.\d+)?)([a-z]+|%)$/, buildDeclaration('width')],
+    [/^(min-|max-)?w-(\d+(?:\.\d+)?)([a-z]+|%)?$/, buildDeclaration('width')],
 ];
